a13a4f453fa89c5f6894bf9a6c1606078d87a71d
Ashish M <mahajan134@gmail.com>
2024-11-26 00:42:34 -0600

    precommit linter updates

--- week5/workspace_utils.py
+++ week5/workspace_utils.py
@@ -1,6 +1,3 @@
-from databricks.sdk import WorkspaceClient
-
-
 def check_if_endpoint_exists(client, endpoint_name):
     try:
         client.serving_endpoints.get(endpoint_name)
@@ -23,7 +20,7 @@ def get_latest_entity_version(client, endpoint_name):
 
 def get_model_entity_index(client, endpoint_name, full_model_name) -> int:
     serving_endpoint = client.serving_endpoints.get(endpoint_name)
-    model_index : int = -1 # endpoint only serving one model
+    model_index: int = -1  # endpoint only serving one model
     for idx, entity in enumerate(serving_endpoint.config.served_entities):
         if entity.entity_name == full_model_name:
             model_index = int(idx)
@@ -31,11 +28,10 @@ def get_model_entity_index(client, endpoint_name, full_model_name) -> int:
     return model_index
 
 
-def get_env_config_file(env:str) -> str:
+def get_env_config_file(env: str) -> str:
     if env == "dev":
         return "project_config_dev.yml"
     elif env == "prod":
         return "project_config_prod.yml"
     else:
         raise ValueError(f"Invalid environment: {env}")
-
